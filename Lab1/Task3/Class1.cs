531381a4b3da3fa1f9348b347e830e12bad78370
nalek0 <belash.aleksandr777@gmail.com>
2023-09-12 23:14:06 +0300

    Task 3

---
+++ Lab1/Task3/Class1.cs
@@ -0,0 +1,47 @@
+﻿namespace Task3
+{
+    internal class MyStask
+    {
+
+        private LinkedList<int> stack;
+
+        private LinkedList<int> minStack;
+
+        public MyStask()
+        {
+            this.stack = new LinkedList<int>();
+            this.minStack = new LinkedList<int>();
+        }
+
+        public void Add(int value)
+        {
+            stack.AddLast(value);
+
+            if (minStack.Last == null)
+            {
+                minStack.AddLast(value);
+            } else
+            {
+                minStack.AddLast(Math.Min(minStack.Last.Value, value));
+            }
+        }
+
+        public void Pop()
+        {
+            minStack.RemoveLast();
+            stack.RemoveLast();
+        }
+
+        public int GetMinValue()
+        {
+            if (minStack.Last == null)
+            {
+                return 0;
+            } else
+            {
+                return minStack.Last.Value;
+            }
+        }
+
+    }
+}
