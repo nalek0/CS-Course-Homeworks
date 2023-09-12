a0a073be0274c0ef85607fcac5a9301de12b7727
nalek0 <belash.aleksandr777@gmail.com>
2023-09-12 21:08:12 +0300

    Task1

---
+++ Lab1/Task1/Task1/MyHashTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class MyHashTable
+    {
+
+        private static int SIZE = 1000;
+
+        private LinkedList<object>[] table;
+
+        public MyHashTable()
+        {
+            this.table = new LinkedList<object>[SIZE];
+            
+            for (int i = 0; i < SIZE; i++)
+            {
+                this.table[i] = new LinkedList<object>();
+            }
+        }
+
+        public bool Add(object obj)
+        {
+            if (Contains(obj))
+            {
+                return false;
+            } else
+            {
+                int objectHash = obj.GetHashCode();
+                int tableIndex = objectHash % SIZE;
+                table[tableIndex].AddLast(obj);
+
+                return true;
+            }
+        }
+
+        public bool Remove(object obj)
+        {
+            int objectHash = obj.GetHashCode();
+            int tableIndex = objectHash % SIZE;
+            
+            return table[tableIndex].Remove(obj);
+        }
+
+        public bool Contains(object obj)
+        {
+            int objectHash = obj.GetHashCode();
+            int tableIndex = objectHash % SIZE;
+
+            return table[tableIndex].Contains(obj);
+        }
+
+    }
+}
