6116ac8df81ab5ec6ed982b289d1660a61de1213
nalek0 <belash.aleksandr777@gmail.com>
2023-09-20 18:34:55 +0300

    Task 2

---
+++ Lab2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task2
+{
+
+    public static class MainClass
+    {
+        
+        public static void Main()
+        {
+            var tripple = new ImmutableTripple(1, 2, 3);
+            var tripple1 = tripple.ChangeFirst(0);
+            var tripple2 = tripple.ChangeSecond(0);
+            var tripple3 = tripple.ChangeThird(0);
+            var tripple000 = tripple.ChangeFirst(0).ChangeSecond(0).ChangeThird(0);
+
+            Console.WriteLine(tripple);
+            Console.WriteLine(tripple1);
+            Console.WriteLine(tripple2);
+            Console.WriteLine(tripple3);
+            Console.WriteLine(tripple000);
+        }
+
+    }
+
+}
