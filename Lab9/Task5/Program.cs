065c9117b7aa0b7b059a3bbc31ed85315a6525f7
nalek0 <belash.aleksandr777@gmail.com>
2023-11-16 17:24:27 +0300

    Task#5

---
+++ Lab9/Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿using Task5;
+
+MyCountdownEvent countdownEvent = new MyCountdownEvent(5);
+
+void PrintThreadName()
+{
+    Console.WriteLine(Thread.CurrentThread.Name);
+    countdownEvent.Signal();
+}
+
+List<Thread> initialThreads = new List<Thread>(5)
+{
+    new Thread(PrintThreadName),
+    new Thread(PrintThreadName),
+    new Thread(PrintThreadName),
+    new Thread(PrintThreadName),
+    new Thread(PrintThreadName)
+};
+
+for (int i = 0; i < 5; i++) {
+    initialThreads[i].Name = "InitialThread#" + i;
+}
+
+initialThreads.ForEach(thread => thread.Start());
+countdownEvent.Wait(TimeSpan.FromSeconds(1));
+Console.WriteLine("End");
