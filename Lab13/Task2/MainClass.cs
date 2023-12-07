178459601683480e0b9d7c695d4d75564d597a2d
nalek0 <belash.aleksandr777@gmail.com>
2023-12-07 14:52:47 +0300

    Task 2 refactoring

--- Lab13/Task2/MainClass.cs
+++ Lab13/Task2/MainClass.cs
@@ -9,7 +9,22 @@ namespace Task2
     class Group : ISerializable
     {
 
-        public Group() { }
+        public decimal GroupId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Student> Students { get; set; }
+
+        // no need to serialize this
+        public int StudentsCount { get; set; }
+
+        public Group(decimal groupId, string name, List<Student> students, int studentsCount)
+        {
+            GroupId = groupId;
+            Name = name;
+            Students = students;
+            StudentsCount = studentsCount;
+        }
 
         public Group(SerializationInfo information, StreamingContext context)
         {
@@ -17,8 +32,8 @@ namespace Task2
             Name = information.GetString("Name") ?? "";
             Students = new List<Student>();
 
-            List<decimal> value = (List<decimal>)information.GetValue("Students", typeof(List<decimal>));
-            List<decimal> ids = new List<decimal>(value);
+            var value = information.GetValue("Students", typeof(List<decimal>));
+            List<decimal> ids = (value == null) ? new List<decimal>() : (List<decimal>) value;
 
             foreach (decimal id in ids)
             {
@@ -35,15 +50,6 @@ namespace Task2
 
             StudentsCount = Students.Count;
         }
-
-        public decimal GroupId { get; set; }
-
-        public string Name { get; set; }
-
-        public List<Student> Students { get; set; }
-
-        // no need to serialize this
-        public int StudentsCount { get; set; }
 
         void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
         {
@@ -71,15 +77,15 @@ namespace Task2
     class Student
     {
 
-        public decimal StudentId { get; set; }
+        public required decimal StudentId { get; set; }
 
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
 
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
-        public int Age { get; set; }
+        public required int Age { get; set; }
 
-        public Group Group { get; set; }
+        public required Group Group { get; set; }
 
         public override string ToString()
         {
@@ -94,8 +100,8 @@ namespace Task2
 
         public static void Main(string[] args)
         {
-            Group x = new Group { GroupId = 1, Name = "group#1", Students = new List<Student>(), StudentsCount = 0 };
-            Group y = new Group { GroupId = 2, Name = "group#2", Students = new List<Student>(), StudentsCount = 0 };
+            Group x = new Group(1, "group#1", new List<Student>(), 0);
+            Group y = new Group(2, "group#2", new List<Student>(), 0);
             Student a = new Student { StudentId = 1, FirstName = "a", LastName = "aa", Age = 20, Group = x };
             Student b = new Student { StudentId = 2, FirstName = "b", LastName = "bb", Age = 20, Group = x };
             Student c = new Student { StudentId = 3, FirstName = "c", LastName = "cc", Age = 20, Group = y };
