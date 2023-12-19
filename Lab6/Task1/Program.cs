eccbd97804aa9224f08f1878de8edbeb27c9c22b
nalek0 <belash.aleksandr777@gmail.com>
2023-12-19 16:28:54 +0300

    Task 1

---
+++ Lab6/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1;
+
+
+public class Program
+{
+
+    private static void PrintLake(int size)
+    {
+        Console.WriteLine("Lake for {0}: ", size);
+        
+        foreach (int value in new Lake(size))
+        {
+            Console.Write("{0} ", value);
+        }
+
+        Console.WriteLine();
+    }
+
+    public static void Main()
+    {
+        PrintLake(5);
+        PrintLake(6);
+        PrintLake(7);
+        PrintLake(8);
+    }
+
+}
