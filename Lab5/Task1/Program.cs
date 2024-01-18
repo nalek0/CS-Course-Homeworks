4ce8ab1aa4dc36168550c3425c0748610db4ca83
nalek0 <belash.aleksandr777@gmail.com>
2024-01-18 18:58:36 +0300

    Task1

---
+++ Lab5/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task1;
+
+public class Program
+{
+
+    private class UserSubscriber : Subscriber
+    {
+
+        public String? Name { get; init; }
+
+        public void OnEvent(string message)
+        {
+            Console.WriteLine($"User({Name}) got message: '{message}'");
+        }
+
+    }
+
+    private class ServerSubscriber : Subscriber
+    {
+
+        public void OnEvent(string message)
+        {
+            Console.WriteLine($"Server got message: '{message}'");
+        }
+
+    }
+
+    public static void Main()
+    {
+        var basePublisher = new Publisher("base");
+        var innerPublisher = new Publisher("inner");
+        EventBus.GetInstance().AddPublisher(basePublisher);
+        EventBus.GetInstance().AddPublisher(innerPublisher);
+
+        var user1 = new UserSubscriber { Name="abc" };
+        var user2 = new UserSubscriber { Name="qqq" };
+        var server = new ServerSubscriber();
+        EventBus.GetInstance().Subscribe("base", server);
+        EventBus.GetInstance().Subscribe("base", user1);
+        EventBus.GetInstance().Subscribe("base", user2);
+        EventBus.GetInstance().Subscribe("inner", server);
+
+        basePublisher.Post("Global message");
+        innerPublisher.Post("Debug message #1");
+        innerPublisher.Post("Debug message #2");
+    }
+
+}
